effcf029dadbdfab864889b7f27adeeb25a5dd15
Malcolm Ahoy <malcolmahoy@gmail.com>
2015-09-25 15:20:51 -0700

    Add webpack configurations and libraries

---
+++ src/js/app.js
@@ -0,0 +1,4 @@
+require('../sass/app.scss');
+
+console.log('hello world!');
+console.log('hello world!');
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var ROOT_PATH = './src/js/';
+var entries = [ path.resolve(ROOT_PATH, 'app.js') ];
+
+
+// dev mode, boot up the dev server and live reload on file changes
+if (process.env.NODE_ENV !== 'production') {
+  entries.push('webpack-dev-server/client?http://localhost:8080');
+}
+
+
+module.exports = {
+  entry: entries,
+  output: {
+    path: './public',
+    publicPath: 'public',
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: [ '', '.js', '.jsx' ]
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('css!sass')
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css', {
+      allChunks: true
+    })
+  ]
+};
