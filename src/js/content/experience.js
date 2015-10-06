2f7276657620adaf8ccf2de6fbef10619387f7ec
Malcolm Ahoy <malcolmahoy@gmail.com>
2015-10-06 16:14:19 -0700

    Make colors across website more consistent
    Added common SASS variables to attempt to make the website colors
    more consistent. Changed the experience section background to white
    for this reason.

--- src/js/content/experience.js
+++ src/js/content/experience.js
@@ -1,7 +1,6 @@
 import '../../sass/experience.scss';
 
 import React from 'react';
-
 import scrollHook from 'simple-scroll-hook';
 
 
@@ -19,45 +18,43 @@ export default class Experience extends React.Component {
     return (
       <section className='experience'>
         <div className='experience__companies hidden' ref='experienceContent'>
-          <div>
-            <h3>Experience</h3>
-            <h4 className='experience__company'>Yola.com</h4>
-            <div className='experience__info clearfix'>
-              <div className='experience__title'>
-                Web Application Developer
-              </div>
-              <div className='experience__duration'>
-                June 2014 - September 2015
-              </div>
+          <h3 className='common__section-title'>Experience</h3>
+          <h4 className='experience__company'>Yola.com</h4>
+          <div className='experience__info clearfix'>
+            <div className='experience__title'>
+              Web Application Developer
             </div>
-            <ul className='experience__duties'>
-              <li>
-                Converted confusing, legacy javascript IIFEs into modern,
-                 well tested modules. Object-oriented models saved state where
-                 appropriate.
-              </li>
-              <li>
-                Engineer and develop client-side javascript libraries,
-                 designed to request for and consume JSON APIs.
-                 This reduced code duplication across applications.
-              </li>
-              <li>
-                Made blocking, synchronous rendering asynchronous with
-                 javascript promises and functional programming, which made
-                 user experience better by displaying HTML elements faster.
-              </li>
-              <li>
-                Led the integration of Segment, a customer data hub,
-                 across all web applications. Allowed Yola to conglomerate data
-                 collection and making integration of other
-                 analytics tools easier.
-              </li>
-              <li>
-                Developed automated dev and build tasks, making development
-                 for other engineers simpler.
-              </li>
-            </ul>
+            <div className='experience__duration'>
+              June 2014 - September 2015
+            </div>
           </div>
+          <ul className='experience__duties'>
+            <li>
+              Converted confusing, legacy javascript IIFEs into modern,
+               well tested modules. Object-oriented models saved state where
+               appropriate.
+            </li>
+            <li>
+              Engineer and develop client-side javascript libraries,
+               designed to request for and consume JSON APIs.
+               This reduced code duplication across applications.
+            </li>
+            <li>
+              Made blocking, synchronous rendering asynchronous with
+               javascript promises and functional programming, which made
+               user experience better by displaying HTML elements faster.
+            </li>
+            <li>
+              Led the integration of Segment, a customer data hub,
+               across all web applications. Allowed Yola to conglomerate data
+               collection and making integration of other
+               analytics tools easier.
+            </li>
+            <li>
+              Developed automated dev and build tasks, making development
+               for other engineers simpler.
+            </li>
+          </ul>
         </div>
       </section>
     );
